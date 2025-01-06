43777f5421296121f459772095efbe70e75a5aa4
dopemukesh <dope.mukeshyadav@gmail.com>
2025-01-06 11:48:25 +0530

    border removed

--- src/components/webComponents/WebHome.jsx
+++ src/components/webComponents/WebHome.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const WebHome = () => {
     return (
         <div className="flex flex-col items-center md:justify-center min-h-svh px-4 max-w-[1024px]">
-            <div className='text-center mt-16 flex flex-col items-center justify-center gap-4 bg-gray-100 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 p-4 rounded-xl dark:shadow-lg'>
+            <div className='text-center mt-16 flex flex-col items-center justify-center gap-4 bg-gray-100 dark:bg-gray-800 p-4 rounded-xl dark:shadow-lg'>
                 <h1 className="text-xl font-bold text-center lg:text-left md:text-4xl">Organize your
                     to-do lists from <span className="bg-gradient-to-r from-purplex-300 to-purplex-500 bg-clip-text text-transparent">anywhere</span></h1>
                 <p className="text-gray-500 dark:text-gray-500 text-center">
